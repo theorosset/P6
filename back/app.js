f424ec46d220c143ee9e9580ca595db3c3ca854f
Théo Rosset <theo.rosset@outlook.fr>
2022-01-14 10:22:19 +0100

    server: changement pour le lancement, installation de mongoose+connection

--- back/app.js
+++ back/app.js
@@ -1,4 +1,29 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+//connection a la base de donnée
+mongoose
+  .connect(process.env.DB_PATH, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  //on recupere une promesse si elle est resolu :
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  //si elle n'est pas resolu
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//info sur tp
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 module.exports = app;
--- back/server.js
+++ back/server.js
@@ -1,2 +1,5 @@
-const http = require("http");
 const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
